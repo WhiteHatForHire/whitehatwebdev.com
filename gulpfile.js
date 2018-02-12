28eac7d5a458a717bd23993296a811becc0856f5
Maxwell Stephens <stephensm2011@gmail.com>
2018-02-12 13:57:07 -0500

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+var bs = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass())
+                .pipe(gulp.dest('css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
 });
--- js/nav.js
+++ js/nav.js
@@ -2,3 +2,10 @@ $('#toggle').click(function() {
   $(this).toggleClass('active');
   $('#overlay').toggleClass('open');
  });
+
+ $('.overlay-menu > ul').click(function() {
+  $('#toggle').toggleClass('active');
+   
+   $('#overlay').toggleClass('open');
+  console.log('working');
+ });
