c4e603f9ad93d3a1f97e7bd7bb5512367065145c
Maxwell Stephens <stephensm2011@gmail.com>
2018-02-14 20:03:18 -0500

    added dist folder and a bunch to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,52 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var bs = require("browser-sync").create();
+var minify = require('gulp-minify');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task("browser-sync", ["sass"], function() {
   bs.init({
     server: {
-      baseDir: "./"
+      baseDir: "./dist"
     }
   });
+});
+
+
+
+gulp.task('compress', function() {
+  gulp.src('js/*.js')
+    .pipe(minify({
+        ext:{
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('minify', function() {
+  return gulp.src('./html/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task("sass", function() {
   return gulp
     .src("./sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"))
-    .pipe(bs.reload({ stream: true }));
+    .pipe(bs.reload({ stream: true }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task("watch", ["browser-sync"], function() {
+gulp.task("watch", ["browser-sync", "sass", "minify", "compress"], function() {
   gulp.watch("./sass/**/*.scss", ["sass"]);
+  gulp.watch("./html/*.html", ["minify"]);
+  gulp.watch("js/*.js", ["compress"]);
   gulp.watch("*.html").on("change", bs.reload);
   gulp.watch("*.scss").on("change", bs.reload);
+  gulp.watch("*.js").on("change", bs.reload);
 });
